11df28f369bbcce60cc389c9fdeb6aefd37059d2
Ivanildo Junior <101154477+0dL1n@users.noreply.github.com>
2023-08-20 11:31:34 -0300

    Update aula1exemplo1.c

--- aula1/aula1exemplo1.c
+++ aula1/aula1exemplo1.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 
 int main(){
-	float x, y, funcao;
-	printf("\nCalcular f(x) = 3x^2 + 5x + 30 ");
-	printf("\ndigiter o valor de x");
-	scanf("%f", &x);
- 	funcao = 3*x*x + 5*x + 30;
-	prinf("%f", funcao);
+	char nome[100];
+	printf("qual o seu nome?\n");
+	scanf("%s", &nome);
+	printf("olah, %s ", nome);
 	return 0;
 }
